19069536929d12e40f36eccdb3e6b921797a74df
julia-fu0528 <wanjia_fu@brown.edu>
2025-01-17 23:18:12 -0500

    retrain franka

--- scripts/network.py
+++ scripts/network.py
@@ -35,18 +35,18 @@ class JointNetwork(nn.Module):
                 nn.Linear(64, 128),
                 nn.ReLU(),
 
-                nn.Linear(128, 128),
+                nn.Linear(128, 256),
                 nn.ReLU(),
-                # nn.Linear(256, 256),
-                # nn.ReLU(),
-                # nn.Dropout(0.3),
-                # nn.Linear(256, 256),
-                # nn.ReLU(),
+                nn.Linear(256, 256),
+                nn.ReLU(),
+                nn.Dropout(0.3),
+                nn.Linear(256, 256),
+                nn.ReLU(),
                 
-                # nn.Linear(256, 256),
-                # nn.ReLU(),
-                # nn.Linear(256, 128),
-                # nn.ReLU(),
+                nn.Linear(256, 256),
+                nn.ReLU(),
+                nn.Linear(256, 128),
+                nn.ReLU(),
 
                 nn.Linear(128, output_dim)
             )
