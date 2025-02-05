b645c45e757d5f84f5a50d846bed801b8c2ad266
Julia Fu <wanjia_fu@brown.edu>
2025-02-04 19:04:05 -0500

    spot

--- scripts/network.py
+++ scripts/network.py
@@ -41,10 +41,12 @@ class JointNetwork(nn.Module):
 
                 nn.Linear(64,128), 
                 nn.ReLU(),
+                nn.Dropout(0.3),
                 nn.Linear(128, 256),
                 nn.ReLU(),
                 nn.Linear(256, 128),
                 nn.ReLU(),
+                nn.Dropout(0.3),
                 nn.Linear(128, 128),
                 nn.ReLU(),
 
