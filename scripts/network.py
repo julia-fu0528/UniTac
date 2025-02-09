fbed61807ade613860319717c0d1a1f1572cfaed
Julia Fu <wanjia_fu@brown.edu>
2025-02-09 17:02:57 -0500

    dropout

--- scripts/network.py
+++ scripts/network.py
@@ -51,7 +51,7 @@ class JointNetwork(nn.Module):
 
                 nn.Linear(256, 128),
                 nn.ReLU(),
-                nn.Dropout(0.3),
+                # nn.Dropout(0.3),
                 nn.Linear(128, 128),
                 nn.ReLU(),
                 nn.Dropout(0.3),
