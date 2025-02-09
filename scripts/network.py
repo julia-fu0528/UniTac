a72ebec23702beddb902cbfbbf0b396d29f745fd
Julia Fu <wanjia_fu@brown.edu>
2025-02-09 17:06:20 -0500

    network

--- scripts/network.py
+++ scripts/network.py
@@ -41,7 +41,7 @@ class JointNetwork(nn.Module):
 
                 nn.Linear(64,128), 
                 nn.ReLU(),
-                # nn.Dropout(0.3),
+                # nn.Dropout(0.1),
                 nn.Linear(128, 128),
                 nn.ReLU(),
                 # nn.Dropout(0.3),
@@ -49,10 +49,10 @@ class JointNetwork(nn.Module):
                 nn.ReLU(),
                 nn.Dropout(0.3),
 
-                nn.Linear(256, 128),
+                nn.Linear(256, 256),
                 nn.ReLU(),
-                # nn.Dropout(0.3),
-                nn.Linear(128, 128),
+                nn.Dropout(0.3),
+                nn.Linear(256, 128),
                 nn.ReLU(),
                 nn.Dropout(0.3),
 
