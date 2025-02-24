6e23d02620920f499c195443837d727db52cd38c
Julia Fu <wanjia_fu@brown.edu>
2025-02-23 19:39:27 -0500

    run

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,15 +6,15 @@
 #                             # --hostname 138.16.161.22 \
 
 
-echo "################# DATALOADER ###############################"
-python dataset.py --session gouger1209 --data_dir ../data \
-                    --markers_path ../data/gouger_markers_pos.txt \
-                    --seq 1  --robot_type spot  \
+# echo "################# DATALOADER ###############################"
+# python dataset.py --session gouger1209 --data_dir ../data \
+#                     --markers_path ../data/gouger_markers_pos.txt \
+#                     --seq 1  --robot_type spot  \
 
-echo "################# TRAINING ###############################"
-python train.py --session gouger1209 --data_dir ../data \
-                --markers_path ../data/gouger_markers_pos.txt --device "gpu" \
-                --seq 1 --robot_type spot 
+# echo "################# TRAINING ###############################"
+# python train.py --session gouger1209 --data_dir ../data \
+#                 --markers_path ../data/gouger_markers_pos.txt --device "gpu" \
+#                 --seq 1 --robot_type spot 
 
 
 echo "################# PREDICTING ################################"
